6a9eaca314c177ba5ef637bf3dc95eb94373d8b1
root <root@LAPTOP-HT7B13IA.localdomain>
2020-06-16 09:53:13 -0300

    a

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+  string mensagem;
+  //$./prog add
+  if (argc == 2 && !strcmp(argv[1], "add")){
+    cout << "mensagem:" ;
+    cin >> mensagem;
+    cout << "Mensagem adicionada:" << mensagem << ".\n";
+    return -1;
+  }
+  //$./prog ou qualquer outra entrada q difira do padrão $./prog add mensagem ou $./prog add
+  if (argc != 3 ){
+    cout << "Uso: " << argv[0] << " add <mensagem>" << ".\n";
+    return -1;
+  }
+  //$./prog add mensagem
+  cout << "Mensagem adicionada:" << argv[2]  << ".\n";
+  return 0;
+}
