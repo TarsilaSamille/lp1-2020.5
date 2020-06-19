9dd4143a7c41703172ef003563510b3ffefa6ae1
root <root@LAPTOP-HT7B13IA.localdomain>
2020-06-19 13:58:41 -0300

    atividade

--- aula3 copy/main.cpp
+++ aula3 copy/main.cpp
@@ -2,23 +2,27 @@
 #include <fstream>
 #include <string>
 #include <cstring>
+#include <ctime>
+#include <sstream>
+
 using namespace std;
 
 void mostrar_uso(string nome_prog);
 void mostrar_mensagem(string mensagem);
 int gravar_mensagem(string mensagem);
+bool data_existe();
+string format_current_date(string format);
+string get_current_date();
+string get_current_time();
 int listar_mensagens();
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char * argv[]) {
   string nome_prog = argv[0];
   string action = (argv[1] == NULL) ? " " : argv[1];
   string mensagem;
-  if (argc == 2)
-  {
+  if (argc == 2) {
     //$./prog add
-    if (action == "add")
-    {
+    if (action == "add") {
       cout << "mensagem:";
       cin >> mensagem;
       mostrar_mensagem(mensagem);
@@ -26,14 +30,12 @@ int main(int argc, char *argv[])
       return -1;
     }
     //$./prog listar
-    if (action == "list")
-    {
+    if (action == "list") {
       return listar_mensagens();
     }
   }
   //$./prog ou qualquer outra entrada q difira do padrão $./prog add mensagem ou $./prog add
-  if (argc != 3)
-  {
+  if (argc != 3) {
     mostrar_uso(nome_prog);
     return -1;
   }
@@ -42,53 +44,77 @@ int main(int argc, char *argv[])
   return gravar_mensagem(argv[2]);
 }
 
-void mostrar_uso(string nome_prog)
-{
+void mostrar_uso(string nome_prog) {
   cout << "Uso: " << nome_prog << " list" << "\n";
   cout << "Uso: " << nome_prog << " add <mensagem>" << "\n";
 }
 
-void mostrar_mensagem(string mensagem)
-{
+void mostrar_mensagem(string mensagem) {
   cout << "Mensagem adicionada:" << mensagem << "\n";
 }
 
-int gravar_mensagem(string mensagem)
-{
-  ofstream arquivo;
+int gravar_mensagem(string mensagem) {
+  fstream arquivo;
   arquivo.open("mensagem.txt", fstream::app);
-  if (!arquivo.is_open())
-  {
+  if (!arquivo.is_open()) {
     cerr << "Arquivo não existente ou sem permissão de leitura.";
     return -1;
   }
-  arquivo << mensagem << "\n";
+  if (!data_existe()) {
+    if (arquivo.tellg() != 0){arquivo << "\n";}
+    arquivo << "#" << get_current_date() << "\n\n";
+  }
+  arquivo << "- " << get_current_time() << " " << mensagem << "\n";
   arquivo.close();
   return 0;
 }
 
-int listar_mensagens()
-{
+bool data_existe() {
+  fstream arquivo;
+  arquivo.open("mensagem.txt", fstream:: in | fstream::out | fstream::app);
+  string message;
+  while (!arquivo.eof()) {
+    getline(arquivo, message);
+    if (message.rfind("#", 0) == 0) {
+      if (message.rfind(get_current_date(), 1) == 1) {
+        return true;
+      }
+    }
+    if (message.size() == 0) {
+      continue;
+    }
+  }
+  return false;
+}
+
+string format_current_date(string format) {
+  time_t now = time(nullptr);
+  char result[1024];
+  strftime(result, sizeof(result), format.c_str(), localtime( & now));
+  return string(result);
+}
+string get_current_date() {
+  return format_current_date("%d/%m/%Y");
+}
+string get_current_time() {
+  return format_current_date("%H:%M:%S");
+}
+
+int listar_mensagens() {
   ifstream arquivo;
   arquivo.open("mensagem.txt");
-  if (!arquivo.is_open())
-  {
+  if (!arquivo.is_open()) {
     cerr << "Arquivo não existente ou sem permissão";
     return -1;
   }
-
   string message;
   int line_number = 0;
-  while (!arquivo.eof())
-  {
-    ++line_number;
+  while (!arquivo.eof()) {
     getline(arquivo, message);
-
-    if (message.size() == 0)
-    {
+    if (message.size() == 0 || (message.rfind("#", 0) == 0)) {
       continue;
     }
-    cout << line_number << ". " << message << "\n";
+    cout << message.erase(0, 11) << "\n";
   }
   arquivo.close();
   return 0;
